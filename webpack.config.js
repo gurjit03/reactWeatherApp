9d7832ab67087c0b58c8f3b4b55f6926bd7c638c
gurjit03 <gurjitmehta@gmail.com>
2017-01-30 13:25:39 +0530

    changed css to scss

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
       About : 'app/components/About.jsx',
       Examples : 'app/components/Examples.jsx',
       OpenWeatherMap : 'app/api/OpenWeatherMap.jsx',
-      applicationStyles : 'app/styles/app.css'
+      applicationStyles : 'app/styles/app.scss'
     },
     extensions : ['','.js','.jsx']
   },
